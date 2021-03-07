aaec77dfb4ab07f8eaed3c2653f17798ffa71557
Ferenc1980 <hefike30@gmail.com>
2021-03-07 11:29:02 +0100

    Update kivetelek.php

--- kivetelek.php
+++ kivetelek.php
@@ -1,1 +1,39 @@
-<p>...kivételes napok</p>
+<?php
+print_r( $_POST);
+
+require_once "config.php";
+$msg="";
+
+
+if(isset($_POST["mentes"])){
+    extract($_POST);
+    
+    
+    $sql="insert into film values (null,'{$datum}','{$unnepnap}','{$munkanap}')";
+    echo $sql;
+    $stmt=$db->exec($sql);
+    if($stmt){
+        $msg="Sikeres adatbeírás!";
+    }else{
+        $msg="HIBA!";
+    }
+}
+
+
+?>
+
+
+<h1>Munka és munkaszüneti napok bevezetése:</h1>
+<form method="post">
+    <input type="date" name="datum" id="" required>
+    <input type="checkbox" name="unnepnap" id="">Ünnepnap?
+    <input type="checkbox" name="munkanap" id="">Munkanap?
+    <input type="submit" name="mentes" value="Mentés">
+    
+</form>
+
+<div>
+
+<?=$msg?>
+
+</div>
